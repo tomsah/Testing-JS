9e098bb478cbafe6963761efdd186c71653ca0d4
Thomas Salah <thomas.salah@gmail.com>
2020-04-23 17:29:32 +0100

    Core: install jest-dom and set it up to be loaded with each files

--- jest-cypress-react-babel-webpack/jest.config.js
+++ jest-cypress-react-babel-webpack/jest.config.js
@@ -19,6 +19,8 @@ module.exports = {
     //return the mock for the style module when jest see .css files
     '\\.css$': require.resolve('./test/style-mock.js')
   },
+  //files that jest will run after it sets up the jest testing env
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   // necessary if you use your css in js (emotion, css-js, glamorous ...)
   // to see the css added in your snapshot
   snapshotSerializers: ['jest-emotion']
