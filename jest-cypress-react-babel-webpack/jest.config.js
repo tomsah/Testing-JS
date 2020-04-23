fed644ec6790a234f62ff1a1d99934e0b0ec6308
Thomas Salah <thomas.salah@gmail.com>
2020-04-23 15:56:39 +0100

    Config: adding jest serialiser for emotion + config

--- jest-cypress-react-babel-webpack/jest.config.js
+++ jest-cypress-react-babel-webpack/jest.config.js
@@ -12,5 +12,8 @@ module.exports = {
     // pattern) like if it was a commonjs module
     //return the mock for the style module when jest see .css files
     '\\.css$': require.resolve('./test/style-mock.js')
-  }
+  },
+  // necessary if you use your css in js (emotion, css-js, glamorous ...)
+  // to see the css added in your snapshot
+  snapshotSerializers: ['jest-emotion']
 }
