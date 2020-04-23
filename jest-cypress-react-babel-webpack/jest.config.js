41e323e093002bd2c61d123067ed6459f9a2c421
Thomas Salah <thomas.salah@gmail.com>
2020-04-23 15:49:06 +0100

    Config: setting up jest for css module

--- jest-cypress-react-babel-webpack/jest.config.js
+++ jest-cypress-react-babel-webpack/jest.config.js
@@ -2,6 +2,12 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom', // this is to simulate the DOM
   // on a node environment
   moduleNameMapper: {
+    // CONFIG necessary fif you use css module
+    //return a string for the path that was accessed for that module
+    // essentially return the name of the comp as a class name, allowing
+    // testing around classname logic ect.. essential because we are mocking
+    // our css module
+    '\\.module\\.css$': 'identity-obj-proxy',
     // jest try to require our css files ( because of import foo from foo.css
     // pattern) like if it was a commonjs module
     //return the mock for the style module when jest see .css files
